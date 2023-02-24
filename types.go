71bcdc5aa22588bbf5c24d92c54886a17bdaada0
37LIB <114300648+37LIB@users.noreply.github.com>
2023-02-24 18:16:16 +0000

    Update week1_technical.html

--- types.go
+++ types.go
@@ -8,4 +8,46 @@ type Document struct {
 	Text   string   `json:"text"`
 	Tokens []*Token `json:"tokens"`
 	Spans  []*Span  `json:"spans"`
-	Lang   string   `json:"
+	Lang   string   `json:"lang"`
+	Langs  []string `json:"langs"`
+}
+
+type TokenType byte
+
+//go:generate jsonenums -type=TokenType
+//go:generate stringer -type=TokenType
+const (
+	EOF TokenType = iota
+	Space
+	Symbol
+	Number
+	Letters
+	Punct
+	Word
+)
+
+type Token struct {
+	Doc         *Document         `json:"-"`
+	Text        string            `json:"text"`
+	Type        TokenType         `json:"type"`
+	Script      string            `json:"script"`
+	I           int               `json:"i"`
+	StartByte   int               `json:"start_byte"`
+	EndByte     int               `json:"end_byte"`
+	Annotations map[string]string `json:"annotations"`
+}
+
+func (t *Token) String() string {
+	return t.Text
+}
+
+type Span struct {
+	Doc         *Document              `json:"-"`
+	Start       int                    `json:"start"`
+	End         int                    `json:"end"`
+	Annotations map[string]interface{} `json:"annotations"`
+}
+
+func (s *Span) String() string {
+	output := bytes.Buffer{}
+	f
