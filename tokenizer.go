2013a7b0e35588c027e96011fda6be97b7131201
37LIB <114300648+37LIB@users.noreply.github.com>
2023-02-24 11:00:11 +0000

    fix cabal file

--- tokenizer.go
+++ tokenizer.go
@@ -16,4 +16,45 @@ func init() {
 
 func Type(text string) TokenType {
 	switch {
-	case util.StringIs
+	case util.StringIs(text, unicode.IsSpace):
+		return Space
+	case util.StringIs(text, unicode.IsSymbol):
+		return Symbol
+	case util.StringIs(text, unicode.IsNumber):
+		return Number
+	case util.StringIs(text, unicode.IsPunct):
+		return Punct
+	//case util.StringIs(text, unicode.IsLetter):
+	case util.StringIs(text, func(r rune) bool {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') {
+			return false
+		}
+		return true
+	}):
+		return Letters
+	}
+	return Word
+}
+
+func Script(text string) string {
+	for k, v := range unicode.Scripts {
+		if util.StringIs(text, func(r rune) bool { return unicode.Is(v, r) }) {
+			return k
+		}
+	}
+	return "Unknown"
+}
+
+type Tokenizer struct {
+}
+
+func (t *Tokenizer) Process(d *Document) error {
+	if d == nil || len(d.Text) == 0 {
+		return nil
+	}
+	var tokens []*Token
+	var pos int
+	for i, item := range tokenizer.TokenizePro(d.Text) {
+		word := item.Text
+		l := len([]byte(word))
+		token := &Token{Doc: d, Text: word, Type: Type(word
